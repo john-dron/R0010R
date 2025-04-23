9ef4e0ee87e40be52c9e6118825bcd3f17704d72
John <john01carlsson@gmail.com>
2025-04-23 13:18:52 +0200

    fixed file name for sensor and added som function that are useful in Satellite.py

--- Satellite.py
+++ Satellite.py
@@ -1,1 +1,13 @@
 #This is the main file of the sattelite with all its functions
+from Sensor.py import sensorData
+
+def sensorHandle():
+    print("do something")
+
+def commsWithGS():
+    #Write to file with intervalls as to simulate the communication with the groundstation
+    print("temp")
+
+def modeChange():
+    #change the modes i.e. afe mmode, boot mode etc.
+    print("temp")
---
+++ Sensor.py
@@ -0,0 +1,3 @@
+#This is the sensor where data will be produced, still figuring out hwhat that should be
+def sensorData(data):
+    print("some data here")
