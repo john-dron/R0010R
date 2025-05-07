9dc06e0261e7102a31fcd04aa253eda0494953df
John <john01carlsson@gmail.com>
2025-05-07 16:26:02 +0200

    Fixed a mode handler and basics of modes

--- GroundStation.py
+++ GroundStation.py
@@ -1,6 +1,8 @@
 #This is the main file for the ground station
 from Crypto.Cipher import AES
 import hashlib
+import os
+import time
 
 def unpad(data):
     pad_len = data[-1]
@@ -26,8 +28,20 @@ def decrypt_file(encrypted_file: str, output_file: str, passphrase: str):
     print(f"'{encrypted_file}' decrypted and saved as '{output_file}'.")
 
 # Usage
-decrypt_file('coms.aes', 'data_decrypted.csv', 'RackarnsRabarber')
+def main():
+    while(True):
+        if (os.path.isfile('coms1.aes')):
+            for i in range(1,4):
+                decrypt_file(f'coms{i}.aes', f'data_decrypted{i}.csv', 'RackarnsRabarber')
+                os.remove(f'coms{i}.aes')
+        else:
+            print("No current uplink. Waiting...")
+        time.sleep(5)
 
+
+
+if (__name__ == "__main__"):
+    main()
 #def readComms():
     #define how to read from the comms file
 #    print("test")
--- Satellite.py
+++ Satellite.py
@@ -3,7 +3,15 @@ import os
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 import hashlib
+import time
 from Sensor import generate_and_save_numbers
+from particlesens import simulate_optical_particle_sensor
+from Spectrometer import simulate_spectrometer_sensor
+
+currentMode='boot'
+lastMode=''
+battery=100
+solar_check=False
 
 def pad(data):
     pad_len = 16 - len(data) % 16
@@ -27,16 +35,112 @@ def CommsWithGS(input_file: str, output_file: str, passphrase: str):
     os.remove(input_file)
     print(f"'{input_file}' encrypted and saved as '{output_file}', then deleted.")
 
-
-def main():
-
-
-    # Usage
-    CommsWithGS('data.csv', 'coms.aes', 'RackarnsRabarber')    
+def boot():
+    print("deploying solar panel\n")
+    global currentMode
+    global battery
+    global lastMode
+    lastMode=currentMode
+    time.sleep(1)
+    print("cheching battery power\n")
+    if(battery<=20):
+        print("low battery entering safe mode to conserve power\n")
+        currentMode='safe'
+        return
+    else:
+        print("deployment complete entering!\n")
+        currentMode='standby'
+        return
     
+
+def standby():
+    print("standby awaiting instructions\n")
+    global solar_check
+    global currentMode
+    global lastMode
+    lastMode=currentMode
+    if solar_check:
+        print("On day light side, preparing to gather data!\n")
+        currentMode = 'nominal'
+    else:
+        time.sleep(5)
+        solar_check=True
+    return
+
+def nominal():
+    global currentMode
+    global solar_check
+    print("gathering senosordata 1")
+    generate_and_save_numbers()
+    print("gathering particle data")
+    simulate_optical_particle_sensor()
+    print("gathering spectroscopic data\n")
+    simulate_spectrometer_sensor()
+    currentMode='downlink'
+    solar_check=False
+    return
+
+
+def downlink():
+    # Usage
+    global currentMode
+    print("Sending encrypted data to Ground Station!!!")
+    CommsWithGS('data.csv', 'coms1.aes', 'RackarnsRabarber')
+    CommsWithGS('orbital_sensor_data.csv', 'coms2.aes', 'RackarnsRabarber')
+    CommsWithGS('spectrometer_data.csv', 'coms3.aes', 'RackarnsRabarber')
+    currentMode='standby'
+
+
+def safe():
+    global battery
+    global currentMode
+    global lastMode
+    print("safe mode")
+    if battery<=20:
+        for i in range(battery,100):
+            battery+=1
+            time.sleep(0.1)
+        print(f"battery now recharged returning to {lastMode} mode")
+        currentMode = lastMode
+        return
+    else:
+        currentMode='standby'
+        return
+        
+            
+
+
+def modeChange(mode):
+    #change the modes i.e. safe mmode, boot mode etc.
+    match mode:
+            case 'boot':
+                print(f"entering {mode} mode\n")
+                boot()
+            case 'standby':
+                print(f"entering {mode} mode\n")
+                standby()
+            case 'nominal':
+                print(f"entering {mode} mode\n")
+                nominal()
+            case 'downlink':
+                print(f"entering {mode} mode\n")
+                downlink()
+            case 'safe':
+                print(f"entering {mode} mode\n")
+                safe()
+            case _:
+                print(f"Unknown mode: '{mode}' entering safe mode instead")
+                modeChange('safe')
+    return
+            
+def main():
+    while(True):
+        modeChange(currentMode)
+        print(currentMode)
+        print("\n")
+        time.sleep(2)
+
+
 if (__name__ == "__main__"):
     main()
 
-#def modeChange():
-    #change the modes i.e. afe mmode, boot mode etc.
-#    print("temp")
